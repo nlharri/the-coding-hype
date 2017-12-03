88e8a236d63e5f270ebff877d2977bae4f1b11a8
nlharri <thecodinghype>
2017-12-04 00:44:28 +0100

    Cleanup
    Cleanup

--- react-hello-world/main.js
+++
@@ -1,9 +0,0 @@
-import { PythagorasTree } from './PythagorasTree.js'
-
-ReactDOM.render(
-  // React.createElement(PythagorasTree, {
-  //   sprout: 0.01,
-  //   sway: 0.04,
-  // }),
-  document.getElementById('app')
-)
